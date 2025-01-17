39138223609d16007b16cfdfd1cc4f1514fedef9
jootaavee <jvfs55007@gmail.com>
2025-01-16 22:19:13 -0300

    ajuste tela 2

--- src/pages/tela2.tsx
+++ src/pages/tela2.tsx
@@ -6,23 +6,6 @@ import Relogio from '../components/relogio.tsx';
 import Data from '../components/data.tsx';
 import Detalhe from '../components/detalhe/detalhe.tsx';
 
-//imagens dos botoes
-// const assu = require('./imagensCentroT2/ASSU/normal.png');
-// const proex = require('./imagensCentroT2/PROEX/normal.png');
-// const epilogo = require('./imagensCentroT2/EPÍLOGO/normal.png');
-// const facem = require('./imagensCentroT2/FACEM/normal.png');
-// const facs = require('./imagensCentroT2/FACS/normal.png');
-// const propeg = require('./imagensCentroT2/PROPEG/normal.png');
-// const reitoria = require('./imagensCentroT2/REITORIA/normal.png');
-// const fad = require('./imagensCentroT2/FAD/normal.png');
-// const fe = require('./imagensCentroT2/FE/normal.png');
-// const fasso = require('./imagensCentroT2/FASSO/normal.png');
-// const fala = require('./imagensCentroT2/FALA/normal.png');
-// const fafic = require('./imagensCentroT2/FAFIC/normal.png');
-// const fanat = require('./imagensCentroT2/FANAT/normal.png');
-
-//ajustar o botao do link teste la em baixo para que n fique diferente dos outros :)
-
 const Tela2 = () => {
     return (<>
         <div className='container_tela_2'>
